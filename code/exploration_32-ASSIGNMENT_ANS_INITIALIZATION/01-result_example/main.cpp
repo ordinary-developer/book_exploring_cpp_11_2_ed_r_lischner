9236f0930e699abaad16473bc0ddfdc45ba79268
ordinary-developer <merely.ordinary.developer@gmail.com>
2016-12-10 15:03:00 +0300

    ch-32: fix the single code sample
    fix (put instead the other code sample) the single code sample
    for exploration 32

--- code/exploration_32-ASSIGNMENT_ANS_INITIALIZATION/01-result_example/main.cpp
+++ code/exploration_32-ASSIGNMENT_ANS_INITIALIZATION/01-result_example/main.cpp
@@ -1,168 +1,182 @@
 #include <cassert>
 #include <cstdlib>
-#include <iostream>
-#include <limits>
+#include <istream>
+#include <ostream>
 #include <sstream>
+
 
 int gcd(int n, int m)
 {
-  n = std::abs(n);
-  while (m != 0) {
-    int tmp{n % m};
-    n = m;
-    m = tmp;
-  }
-  return n;
+    n = std::abs(n);
+    while (m != 0) {
+        int tmp(n % m);
+        n = m;
+        m = tmp;
+    }
+    return n;
 }
+
 
 struct rational
 {
-  rational(int num, int den)
-  : numerator{num}, denominator{den}
-  {
-    reduce();
-  }
-  rational(int num)
-  : numerator{num}, denominator{1}
-  {}
+    rational()
+    : numerator{0}, denominator{1}
+    {/*empty*/}
 
-  void assign(int num, int den)
-  {
-    numerator = num;
-    denominator = den;
-    reduce();
-  }
+    rational(int num)
+    : numerator{num}, denominator{1}
+    {/*empty*/}
 
-
-  void reduce()
-  {
-    assert(denominator != 0);
-    if (denominator < 0)
+    rational(int num, int den)
+    : numerator{num}, denominator{den}
     {
-      denominator = -denominator;
-      numerator = -numerator;
+        reduce();
     }
-    int div{gcd(numerator, denominator)};
-    numerator = numerator / div;
-    denominator = denominator / div;
-  }
 
-  int numerator; 
-  int denominator; 
+    rational(double r)
+    : rational{static_cast<int>(r * 10000), 10000}
+    {/*empty*/}
+
+    rational& operator=(rational const& that)
+    {
+        this->numerator = that.numerator;
+        this->denominator = that.denominator;
+        reduce();
+        return *this;
+    }
+
+    float as_float()
+    {
+        return static_cast<float>(numerator) / denominator;
+    }
+
+    double as_double()
+    {
+        return static_cast<double>(numerator) / denominator;
+    }
+
+    long double as_long_double()
+    {
+        return static_cast<long double>(numerator) / 
+                     denominator;
+    }
+
+    void assign(int num, int den)
+    {
+        numerator = num;
+        denominator = den;
+        reduce();
+    }
+
+    void reduce()
+    {
+        assert(denominator != 0);
+        if (denominator < 0)
+        {
+            denominator = -denominator;
+            numerator = -numerator;
+        }
+        int div{gcd(numerator, denominator)};
+        numerator = numerator / div;
+        denominator = denominator / div;
+    }
+
+    int numerator;
+    int denominator;
 };
 
-bool operator==(rational const& a, rational const& b)
+rational abs(rational const& r)
 {
-  return a.numerator == b.numerator and a.denominator == b.denominator;
-}
-
-inline bool operator!=(rational const& a, rational const& b)
-{
-  return not (a == b);
-}
-
-
-bool operator<(rational const& a, rational const& b)
-{
-  return a.numerator * b.denominator < b.numerator * a.denominator;
-}
-
-
-inline bool operator<=(rational const& a, rational const& b)
-{
-  return not (b < a);
-}
-
-inline bool operator>(rational const& a, rational const& b)
-{
-  return b < a;
-}
-
-
-inline bool operator>=(rational const& a, rational const& b)
-{
-  return not (b > a);
-}
-
-
-rational operator+(rational const& lhs, rational const& rhs)
-{
-  return rational{lhs.numerator * rhs.denominator + rhs.numerator * lhs.denominator,
-                  lhs.denominator * rhs.denominator};
+    return rational{std::abs(r.numerator), r.denominator};
 }
 
 rational operator-(rational const& r)
 {
-  return rational{-r.numerator, r.denominator};
+    return rational{-r.numerator, r.denominator};
 }
 
+rational operator+(rational const& lhs, rational const& rhs)
+{
+    return rational{lhs.numerator * rhs.denominator + rhs.numerator * lhs.denominator,
+                                    lhs.denominator * rhs.denominator};
+}
 
 rational operator-(rational const& lhs, rational const& rhs)
 {
-  return rational{lhs.numerator * rhs.denominator - rhs.numerator * lhs.denominator,
-                  lhs.denominator * rhs.denominator};
+    return rational{lhs.numerator * rhs.denominator - rhs.numerator * lhs.denominator,
+                                    lhs.denominator * rhs.denominator};
 }
 
 rational operator*(rational const& lhs, rational const& rhs)
 {
-  return rational{lhs.numerator * rhs.numerator, lhs.denominator * rhs.denominator};
+    return rational{lhs.numerator * rhs.numerator, lhs.denominator * rhs.denominator};
 }
 
 rational operator/(rational const& lhs, rational const& rhs)
 {
-  return rational{lhs.numerator * rhs.denominator, lhs.denominator * rhs.numerator};
+    return rational{lhs.numerator * rhs.denominator, lhs.denominator * rhs.numerator};
+}
+
+bool operator==(rational const& a, rational const& b)
+{
+    return a.numerator == b.numerator and a.denominator == b.denominator;
+}
+
+inline bool operator!=(rational const& a, rational const& b)
+{
+    return not (a == b);
+}
+
+bool operator<(rational const& a, rational const& b)
+{
+    return a.numerator * b.denominator < b.numerator * a.denominator;
+}
+
+inline bool operator<=(rational const& a, rational const& b)
+{
+    return not (b < a);
+}
+
+inline bool operator>(rational const& a, rational const& b)
+{
+    return b < a;
+}
+
+inline bool operator>=(rational const& a, rational const& b)
+{
+    return not (b > a);
 }
 
 std::istream& operator>>(std::istream& in, rational& rat)
 {
-  int n{0}, d{0};
-  char sep{'\0'};
-  if (not (in >> n >> sep))
-    in.setstate(std::cin.failbit);
-  else if (sep != '/')
-  {
-    in.unget();
-    rat.assign(n, 1);
-  }
-  else if (in >> d)
-    rat.assign(n, d);
-  else
-    in.setstate(std::cin.failbit);
+    int n{0}, d{0};
+    char sep{'\0'};
+    if (not (in >> n >> sep))
+        in.setstate(in.failbit);
+    else if (sep != '/')
+    {
+        in.unget();
+        rat.assign(n, 1);
+    }
+    else if (in >> d)        
+        rat.assign(n, d);
+    else
+        in.setstate(in.failbit);
 
-  return in;
+    return in;
 }
+
 
 std::ostream& operator<<(std::ostream& out, rational const& rat)
 {
-  std::ostringstream tmp{};
-  tmp << rat.numerator;
-  if (rat.denominator != 1)
-    tmp << '/' << rat.denominator;
-  out << tmp.str();
+    std::ostringstream tmp{};
+    tmp << rat.numerator << '/' << rat.denominator;
+    out << tmp.str();
 
-  return out;
+    return out;
 }
 
-bool iofailure(std::istream& in)
-{
-  return in.fail() and not in.bad();
-}
 
-int main()
-{
-  rational r{0};
-
-  while (std::cin)
-  {
-    if (std::cin >> r)
-      std::cout << r << '\n';
-    else if (iofailure(std::cin))
-    {
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits<int>::max(), '\n');
-    }
-  }
-
-  if (std::cin.bad()) 
-    std::cerr << "Unrecoverable input failure\n";
+int main() {
+    return 0;
 }
