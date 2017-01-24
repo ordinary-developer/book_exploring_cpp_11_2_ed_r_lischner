6cd78187657554e5ac4e638994997e87217f15d3
ordinary-developer <merely.ordinary.developer@gmail.com>
2017-01-25 00:48:10 +0300

    ch-58: add the 2-nd example from chapter 58
    add the 2-nd code example from chapter 58
    (topological sort with a graph with artifact indices)

---
+++ code/exploration_58-POINTERS/02-topological_sort_second_draft/artifact.hpp
@@ -0,0 +1,29 @@
+#ifndef ARTIFACT_HPP_
+#define ARTIFACT_HPP_
+
+#include <chrono>
+#include <string>
+
+class artifact {
+    public:
+        typedef std::chrono::system_clock clock;
+
+        artifact(): name_{}, mod_time_{ clock::time_point{}} {}
+        artifact(std::string const& name)
+            : name_{ name }, mod_time_{ get_mod_time() } { }
+
+        std::string const& name() const { return name_; }
+        clock::time_point mod_time() const { return mod_time_; }
+
+        void build() { }
+
+        clock::time_point get_mod_time() {
+            return clock::now();
+        }
+
+    private:
+        std::string name_;
+        clock::time_point mod_time_;
+};
+
+#endif 
