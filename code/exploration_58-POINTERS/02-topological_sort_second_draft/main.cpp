f08a77cf891704fc65f69da4120032b11348e060
ordinary-developer <merely.ordinary.developer@gmail.com>
2017-02-19 18:41:11 +0300

    ch-58: reimplement "pseudo-make" with indicies
    revisit and reimplemen the pseudo-make program with indicies

--- code/exploration_58-POINTERS/02-topological_sort_second_draft/main.cpp
+++ code/exploration_58-POINTERS/02-topological_sort_second_draft/main.cpp
@@ -16,12 +16,12 @@ typedef std::size_t artifact_index;
 
 class dependency_graph {
     public:
-        typedef std::unordered_map<artifact_index, 
-                                   std::unordered_set<artifact_index>>
-        graph_type;
+        typedef 
+          std::unordered_map<artifact_index, 
+                             std::unordered_set<artifact_index>> graph_type;
 
-        void store_dependency(artifact_index target,
-                              artifact_index  dependency) 
+        void store_dependency(artifact_index const& target, 
+                              artifact_index const& dependency)
         {
             graph_[dependency].insert(target);
             graph_[target];
@@ -29,7 +29,7 @@ class dependency_graph {
 
         graph_type const& graph() const { return graph_; }
 
-        template<class OutIter>
+        template <class OutIter>
         void sort(OutIter sorted) const {
             topological_sort(graph_, sorted);
         }
@@ -39,18 +39,21 @@ class dependency_graph {
 };
 
 std::vector<artifact> artifacts{};
-artifact_index lookup_artifact(std::string const& name) {
-    auto iter(std::find_if(artifacts.begin(),
-                           artifacts.end(),
-                           [&name](artifact const& a) {
-                             return a.name() == name; 
-                           })); 
-    if (iter != artifacts.end())
+artifact_index lookup_artifact(std::string const& name) 
+{
+    auto iter = std::find_if(artifacts.begin(), 
+                             artifacts.end(),
+                             [&name](artifact const& a) {
+                               return a.name() == name;
+                             });
+
+    if (artifacts.end() != iter)
         return iter - artifacts.begin();
 
-    artifacts.push_back(artifact{ name });
-        return artifacts.size() - 1;
+    artifacts.push_back(artifact{name});
+    return artifacts.size() - 1;
 }
+
 
 int main() {
     
@@ -59,7 +62,7 @@ int main() {
     std::string line{};
     while (std::getline(std::cin, line)) {
         std::string target_name{}, dependency_name{};
-        std::istringstream stream{ line };
+        std::istringstream stream{line};
         if (stream >> target_name >> dependency_name)
         {
             artifact_index target{ lookup_artifact(target_name) };
@@ -67,7 +70,7 @@ int main() {
             graph.store_dependency(target, dependency);
         }
         else if (not target_name.empty())
-            std::cerr << "malformed input: target, " << target_name 
+            std::cerr << "malformed input: target, " << target_name
                       << ", must be followed by a dependency name\n";
     }
 
@@ -76,11 +79,11 @@ int main() {
         graph.sort(std::back_inserter(sorted));
         for (auto it(sorted.rbegin()), end(sorted.rend()); it != end; ++it)
             std::cout << artifacts.at(*it).name() << '\n';
-    } 
+    }
     catch (std::runtime_error const& ex) {
         std::cerr << ex.what() << '\n';
         return EXIT_FAILURE;
     }
-    
+
     return 0;
 }
