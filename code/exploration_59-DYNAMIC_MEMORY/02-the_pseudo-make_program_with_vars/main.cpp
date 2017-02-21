a600bc4aee3b7461c76da0a46223c5bc37043985
ordinary-developer <merely.ordinary.developer@gmail.com>
2017-02-21 21:10:22 +0300

    ch-58: finish the 2-nd example from chapter 58
    finish the 2-nd example from chapter 58
    (the pseudo-make program with variable support)

--- code/exploration_59-DYNAMIC_MEMORY/02-the_pseudo-make_program_with_vars/main.cpp
+++ code/exploration_59-DYNAMIC_MEMORY/02-the_pseudo-make_program_with_vars/main.cpp
@@ -71,35 +71,53 @@ std::string expand(std::string str) {
     }
 }
 
-
-int main() {
-    dependency_graph graph{};
-
+void parse_graph(std::istream& in, dependency_graph& graph) {
     std::string line{};
-    while (std::getline(std::cin, line)) {
+    while (std::getline(in, line)) {
         std::string target_name{}, dependency_name{};
         std::istringstream stream{line};
         if (stream >> target_name >> dependency_name)
         {
-            artifact* target{ lookup_artifact(target_name) };
-            artifact* dependency{ lookup_artifact(dependency_name) };
+            artifact* target{lookup_artifact(expand(target_name))};
+            artifact* dependency{lookup_artifact(expand(dependency_name))};
             graph.store_dependency(target, dependency);
         }
-        else if (not target_name.empty())
-            std::cerr << "malformed input: target, " << target_name
-                      << ", must be followed by a dependency name\n";
+        else if (not target_name.empty()) {
+            std::string::size_type equal{ target_name.find('=') };
+            if (std::string::npos == equal) {
+                std::cerr << "malformed input: target, " << target_name
+                          << ", must be followed by a dependency name\n";
+            }
+            else {
+                std::string variable_name{ target_name.substr(0, equal) };
+                std::string variable_value{ target_name.substr(equal + 1) };
+                variables[variable_name] = variable_value;
+            }
+        }
     }
+}
+
+
+int main() {
+    dependency_graph graph{};
+
+    parse_graph(std::cin, graph);
 
     try {
-        std::vector<artifact*> sorted{};
+        std::vector<artifact*> sorted;
         graph.sort(std::back_inserter(sorted));
-        for (auto it(sorted.rbegin()), end(sorted.rend()); it != end; ++it)
+
+        for (auto it(sorted.rbegin()), end(sorted.rend());
+             it != end;
+             ++it)
+        {
             std::cout << (*it)->name() << '\n';
+        }
     }
     catch (std::runtime_error const& ex) {
         std::cerr << ex.what() << '\n';
         return EXIT_FAILURE;
     }
-
+    
     return 0;
 }
